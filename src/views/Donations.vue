561e5c12967d825d7520e67c99ee6e3e137c3628
willmt80 <willmt80@gmail.com>
2019-11-10 13:13:07 -0500

    added basic donation page

---
+++ src/views/Donations.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>Contribute to Code 4 Community!</h1>
+    <p>
+      Code 4 Community is powered by student volunteers donating their time and
+      energy to a good cause. We can't do it all on our own, with the help of
+      supporters like you we will be able to grow and take on bigger and better
+      projects. If you want to help out Code 4 Community, please click the
+      button below to donate.
+    </p>
+    <button>Donate</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap");
+@import url("https://fonts.googleapis.com/css?family=Merriweather:400,700&display=swap");
+
+button {
+  background-color: whitesmoke;
+  margin-top: 1em;
+  border-radius: 2px;
+  border: 2px solid rgb(112, 231, 235);
+}
+
+h1 {
+  font-family: "Source Sans Pro", sans-serif;
+  font-size: 24px;
+  font-weight: 600;
+  color: #2c3e50;
+  padding: 5px;
+  margin: 0;
+}
+
+p {
+  font-family: "Merriweather", serif;
+  font-weight: 400;
+  color: black;
+  font-size: 16px;
+  line-height: 32px;
+  margin-right: 30%;
+  margin-left: 30%;
+  text-align: left;
+}
+</style>
