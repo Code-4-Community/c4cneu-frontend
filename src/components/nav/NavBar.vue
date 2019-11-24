5de69d49884da2e72f396b816e4410d402ebe4a7
Brandon Liang <54skyxenon@gmail.com>
2019-11-24 13:47:14 -0500

    Remove duplicate gallery link

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -3,22 +3,12 @@
     <router-link to="/">
       <img src="../../assets/c4c-banner-logo.png" class="home-nav" />
     </router-link>
-    <router-link class="hvr-underline-from-center" to="/news">News</router-link>
-    <router-link class="hvr-underline-from-center" to="/gallery"
-      >Gallery</router-link
-    >
-    <router-link class="hvr-underline-from-center" to="/events"
-      >Events</router-link
-    >
-    <router-link class="hvr-underline-from-center" to="/board"
-      >Board</router-link
-    >
-    <router-link class="hvr-underline-from-center" to="/apply"
-      >Apply</router-link
-    >
-    <router-link class="hvr-underline-from-center" to="/auth"
-      >Sign In</router-link
-    >
+    <router-link to="/news" class="hvr-underline-from-center">News</router-link>
+    <router-link to="/gallery" class="hvr-underline-from-center">Gallery</router-link>
+    <router-link to="/events" class="hvr-underline-from-center">Events</router-link>
+    <router-link to="/board" class="hvr-underline-from-center">Board</router-link>
+    <router-link to="/apply" class="hvr-underline-from-center">Apply</router-link>
+    <router-link to="/auth" class="hvr-underline-from-center">Sign In</router-link>
   </div>
 </template>
 
