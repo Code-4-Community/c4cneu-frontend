a11a8756927aa721b7c27c84d615d6bb5f0faf89
kiminlee2000225 <44308005+kiminlee2000225@users.noreply.github.com>
2020-01-23 22:12:58 -0500

    Added banner images to events, auth, and apply pages. (#46)
    * Added banner images to events, auth, and apply pages.
    
    * changing "past" to "past events"

--- src/views/Apply.vue
+++ src/views/Apply.vue
@@ -1,78 +1,83 @@
 <template>
-  <div class="content">
-    <!-- TODO: ADD PARALLAX -->
-    <h1>Apply to Code4Community</h1>
-    <form>
-      <div class="form-item">
-        <label for="form-name">Name</label>
-        <br />
-        <input type="text" id="form-name" v-model="name" />
+  <div>
+    <section>
+      <div class="parallax" id="plax_7">
+        <h1>Apply to Code4Community</h1>
       </div>
-      <div class="form-item">
-        <label for="year">Year</label>
-        <br />
-        <select v-model="year" id="year">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-        </select>
-      </div>
-      <div class="form-item">
-        <label for="form-major">Major</label>
-        <br />
-        <input type="text" id="form-major" v-model="major" />
-      </div>
-      <div class="form-item">
-        <label for="form-resume">Resume</label>
-        <br />
-        <input
-          type="file"
-          id="form-resume"
-          ref="resume"
-          v-on:change="handleFileUpload()"
-        />
-      </div>
-      <div class="form-item">
-        <label for="areaInterests"
-          >What part of Code4Community are you most interested in?</label
-        >
-        <br />
-        <select v-model="areaInterests" id="areaInterests">
-          <option value="frontend">Frontend</option>
-          <option value="backend">Backend</option>
-          <option value="testing">Testing</option>
-          <option value="security">Security</option>
-          <option value="design">Design</option>
-          <option value="outreach">Outreach</option>
-        </select>
-      </div>
-      <div class="form-item">
-        <label for="form-prior-involvement">
-          Have you had any prior involvement with C4C (e.g. attending
-          workshops)? If so, explain.
-        </label>
-        <br />
-        <textarea id="form-prior-involvement" v-model="priorInvolvement" />
-      </div>
-      <div class="form-item">
-        <label for="form-why-join"
-          >Why do you want to join Code 4 Community?</label
-        >
-        <br />
-        <textarea id="form-why-join" v-model="whyJoin" />
-      </div>
-      <div class="form-item">
-        <button
-          type="submit"
-          :disabled="!validForm()"
-          v-on:click="handleSubmit($event)"
-        >
-          Apply
-        </button>
-      </div>
-    </form>
+    </section>
+    <div class="content">
+      <form>
+        <div class="form-item">
+          <label for="form-name">Name</label>
+          <br />
+          <input type="text" id="form-name" v-model="name" />
+        </div>
+        <div class="form-item">
+          <label for="year">Year</label>
+          <br />
+          <select v-model="year" id="year">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </div>
+        <div class="form-item">
+          <label for="form-major">Major</label>
+          <br />
+          <input type="text" id="form-major" v-model="major" />
+        </div>
+        <div class="form-item">
+          <label for="form-resume">Resume</label>
+          <br />
+          <input
+            type="file"
+            id="form-resume"
+            ref="resume"
+            v-on:change="handleFileUpload()"
+          />
+        </div>
+        <div class="form-item">
+          <label for="areaInterests"
+            >What part of Code4Community are you most interested in?</label
+          >
+          <br />
+          <select v-model="areaInterests" id="areaInterests">
+            <option value="frontend">Frontend</option>
+            <option value="backend">Backend</option>
+            <option value="testing">Testing</option>
+            <option value="security">Security</option>
+            <option value="design">Design</option>
+            <option value="outreach">Outreach</option>
+          </select>
+        </div>
+        <div class="form-item">
+          <label for="form-prior-involvement">
+            Have you had any prior involvement with C4C (e.g. attending
+            workshops)? If so, explain.
+          </label>
+          <br />
+          <textarea id="form-prior-involvement" v-model="priorInvolvement" />
+        </div>
+        <div class="form-item">
+          <label for="form-why-join"
+            >Why do you want to join Code 4 Community?</label
+          >
+          <br />
+          <textarea id="form-why-join" v-model="whyJoin" />
+        </div>
+        <div class="form-item">
+          <button
+            type="submit"
+            :disabled="!validForm()"
+            v-on:click="handleSubmit($event)"
+          >
+            Apply
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
