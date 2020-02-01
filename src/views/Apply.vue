1b5ac2eba4d544fe3693ff341dd3b95c74994d6b
florisdobber <florisdobber@gmail.com>
2020-02-01 11:34:53 -0500

    Initial edits

--- src/views/Apply.vue
+++ src/views/Apply.vue
@@ -5,88 +5,92 @@
         <h1>Apply to Code4Community</h1>
       </div>
     </section>
-    <div class="content">
-      <p>
-        Use the form below to apply to become a contributing member.
-        Contributing members are split up into four teams: Testing & Security,
-        Outreach & Mentorship, Design or Development. Once accepted you will be
-        placed into a team that most closely matches your skills and interests.
-        Most teams will expect their members to have 10 hours available each
-        week to work on C4C related projects and meet once a week to discuss
-        progress and plan work, so make sure you are willing and able to take on
-        that responsibility.
-      </p>
-      <form>
-        <div class="form-item">
-          <label for="form-name">Name</label>
-          <br />
-          <input type="text" id="form-name" v-model="name" />
-        </div>
-        <div class="form-item">
-          <label for="year">Year</label>
-          <br />
-          <select v-model="year" id="year">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-        </div>
-        <div class="form-item">
-          <label for="form-major">Major</label>
-          <br />
-          <input type="text" id="form-major" v-model="major" />
-        </div>
-        <div class="form-item">
-          <label for="form-resume">Resume</label>
-          <br />
-          <input
-            type="file"
-            id="form-resume"
-            ref="resume"
-            v-on:change="handleFileUpload()"
-          />
-        </div>
-        <div class="form-item">
-          <label for="areaInterests"
-            >What part of Code4Community are you most interested in?</label
-          >
-          <br />
-          <select v-model="areaInterests" id="areaInterests">
-            <option value="frontend">Frontend</option>
-            <option value="backend">Backend</option>
-            <option value="testing">Testing</option>
-            <option value="security">Security</option>
-            <option value="design">Design</option>
-            <option value="outreach">Outreach</option>
-          </select>
-        </div>
-        <div class="form-item">
-          <label for="form-prior-involvement">
-            Have you had any prior involvement with C4C (e.g. attending
-            workshops)? If so, explain.
-          </label>
-          <br />
-          <textarea id="form-prior-involvement" v-model="priorInvolvement" />
-        </div>
-        <div class="form-item">
-          <label for="form-why-join"
-            >Why do you want to join Code 4 Community?</label
-          >
-          <br />
-          <textarea id="form-why-join" v-model="whyJoin" />
-        </div>
-        <div class="form-item">
-          <button
-            type="submit"
-            :disabled="!validForm()"
-            v-on:click="handleSubmit($event)"
-          >
-            Apply
-          </button>
-        </div>
-      </form>
+    <div class="row">
+      <div class="column">
+        <form>
+          <div class="form-item">
+            <label for="form-name">Name</label>
+            <br />
+            <input type="text" id="form-name" v-model="name" />
+          </div>
+          <div class="form-item">
+            <label for="year">Year</label>
+            <br />
+            <select v-model="year" id="year">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+          <div class="form-item">
+            <label for="form-major">Major</label>
+            <br />
+            <input type="text" id="form-major" v-model="major" />
+          </div>
+          <div class="form-item">
+            <label for="form-resume">Resume</label>
+            <br />
+            <input
+              type="file"
+              id="form-resume"
+              ref="resume"
+              v-on:change="handleFileUpload()"
+            />
+          </div>
+          <div class="form-item">
+            <label for="areaInterests"
+              >What part of Code4Community are you most interested in?</label
+            >
+            <br />
+            <select v-model="areaInterests" id="areaInterests">
+              <option value="frontend">Frontend</option>
+              <option value="backend">Backend</option>
+              <option value="testing">Testing</option>
+              <option value="security">Security</option>
+              <option value="design">Design</option>
+              <option value="outreach">Outreach</option>
+            </select>
+          </div>
+          <div class="form-item">
+            <label for="form-prior-involvement">
+              Have you had any prior involvement with C4C (e.g. attending
+              workshops)? If so, explain.
+            </label>
+            <br />
+            <textarea id="form-prior-involvement" v-model="priorInvolvement" />
+          </div>
+          <div class="form-item">
+            <label for="form-why-join"
+              >Why do you want to join Code 4 Community?</label
+            >
+            <br />
+            <textarea id="form-why-join" v-model="whyJoin" />
+          </div>
+          <div class="form-item">
+            <button
+              type="submit"
+              :disabled="!validForm()"
+              v-on:click="handleSubmit($event)"
+            >
+              Apply
+            </button>
+          </div>
+        </form>
+      </div>
+      <div class="column">
+        <p>
+          Use the form below to apply to become a contributing member.
+          Contributing members are split up into four teams: Testing & Security,
+          Outreach & Mentorship, Design or Development. Once accepted you will
+          be placed into a team that most closely matches your skills and
+          interests. Most teams will expect their members to have 10 hours
+          available each week to work on C4C related projects and meet once a
+          week to discuss progress and plan work, so make sure you are willing
+          and able to take on that responsibility.
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -164,4 +168,19 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.row {
+  display: flex;
+}
+
+.column {
+  flex: 50%;
+  padding: 2%;
+}
+
+form {
+  padding: 0 0;
+  text-align: center;
+  width: 100%;
+}
+</style>
