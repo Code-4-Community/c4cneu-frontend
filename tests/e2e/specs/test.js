6abe0e4179f0923ffad25ed4cd853f7ac41eabb1
Max Sebso <maxsebso@gmail.com>
2020-03-29 11:38:24 -0700

    Update test.js

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -4,5 +4,6 @@ describe("My First Test", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
     cy.contains("h3", "Changing the world with software");
+    cy.contains("h1", "Mission Statement");
   });
 });
