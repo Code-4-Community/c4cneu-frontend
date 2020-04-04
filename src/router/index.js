3d22d7fdbc67307ef6281049329d472188638898
will <willmt80@gmail.com>
2020-04-03 18:41:33 -0700

    added testing for mounting components with from router from a mock vuex store

--- src/router/index.js
+++ src/router/index.js
@@ -1,71 +1,13 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Landing from "../views/Landing.vue";
-import Auth from "../views/Auth.vue";
-import Events from "../views/Events.vue";
-import EventsAdmin from "../views/EventsAdmin.vue";
-import Board from "../views/Board.vue";
-import News from "../views/News.vue";
-import Post from "../views/Post.vue";
-import Apply from "../views/Apply.vue";
-import EmailSignUp from "../views/EmailSignUp.vue";
+import routes from "./routes";
 
 Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: "/",
-    name: "landing",
-    component: Landing
-  },
-  {
-    path: "/auth",
-    name: "auth",
-    component: Auth
-  },
-
-  {
-    path: "/board",
-    name: "board",
-    component: Board
-  },
-
-  {
-    path: "/events",
-    name: "events",
-    component: Events
-  },
-  {
-    path: "/eventsadmin",
-    name: "eventsadmin",
-    component: EventsAdmin
-  },
-  {
-    path: "/news",
-    name: "news",
-    component: News
-  },
-  {
-    path: "/post/:id",
-    name: "post",
-    component: Post
-  },
-  {
-    path: "/apply",
-    name: "apply",
-    component: Apply
-  },
-  {
-    path: "/emailsignup",
-    name: "signup",
-    component: EmailSignUp
-  }
-];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes: routes
 });
 
 export default router;
